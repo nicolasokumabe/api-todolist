8f188ab1c4c1b04d806ef48912dad278b385e0b6
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-10-13 08:33:08 -0300

    Integração com Banco de Dados

--- todolist/src/main/java/br/com/nicolasokumabe/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/nicolasokumabe/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package br.com.nicolasokumabe.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     
+    @Autowired
+    private IUserRepository userRepository;
+
+
+
+
+
     /**
      * String (texto)
      * Integer (int) numeros inteiros
@@ -27,8 +38,19 @@ public class UserController {
     /*
      * Body
      */
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        
+        if(user != null){
+            System.out.println("Usuário Já existe");
+            // Mensagem de erro
+            // Status Code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário Já existe");
+        }        
+        
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
